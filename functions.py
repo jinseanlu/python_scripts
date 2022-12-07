d322f921a24a8a14a6dceecba6424cb9c7e76ab1
jinseanlu <117480992+jinseanlu@users.noreply.github.com>
2022-12-07 16:36:00 -0500

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,9 @@
+v = True
+value = input("Type in True or False CAPITILIZED first letter: ")
+def checker():
+    if value == "True":
+        print("v is True")
+    elif value == "False":
+        print("v is positively")
+checker()
+

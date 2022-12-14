8b5bc1b62e03dd6f6b8cc22e7dfe09a05a5646d1
jinseanlu <117480992+jinseanlu@users.noreply.github.com>
2022-12-14 15:05:33 -0500

    Create Comparerick.py

---
+++ Comparerick.py
@@ -0,0 +1,23 @@
+#This is a cool project
+#Although I am a starter, I already know these information (I am a computer nerd)
+import random
+x = random.randint(1, 100)
+
+y = random.randint(1, 100)
+
+if x > y:
+    print("x is bigger than y")
+    print(x)
+    print(y)
+    print("https://www.youtube.com/watch?v=nrsnN23tmUA")
+   
+elif y > x:
+    print("y  is bigger than x")
+    print(x)
+    print(y)
+    print("Go to https://www.youtube.com/watch?v=G8iEMVr7GFg")
+elif y == x:
+    print("They are equal!")
+    print(x)
+    print(y)
+    print("Go to https://www.youtube.com/watch?v=oHg5SJYRHA0")

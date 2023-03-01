8c5997af9f6dff53472c005a9514620d210d30d3
jinseanlu <117480992+jinseanlu@users.noreply.github.com>
2023-03-01 16:29:14 -0500

    Create python-turtle-1.py

---
+++ python-turtle-1.py
@@ -0,0 +1,14 @@
+#Licensed with JinSean Lu and Max Fajardo. Find Max on replit on the username MaxFajardo1
+import turtle
+
+t = turtle.Turtle()
+t.pencolor("yellow")
+i=0
+
+t.right(30)
+
+while i<300:
+  i+=1
+  t.goto(1,1)
+  t.right(1)
+  t.forward(100)
